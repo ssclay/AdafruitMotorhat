eb93ecf68de93ffc1105c006077d73d159e1ce39
Scott Clay <sclay@n.io>
2017-08-23 09:24:02 -0600

    Initial Commit

---
+++ tests/test_adafruit_motorhat_block.py
@@ -0,0 +1,19 @@
+from nio.block.terminals import DEFAULT_TERMINAL
+from nio.signal.base import Signal
+from nio.testing.block_test_case import NIOBlockTestCase
+from adafruit_motorhat_block import AdafruitMotorhat
+
+
+class TestAdafruitMotorhat(NIOBlockTestCase):
+
+    def test_process_signals(self):
+        """Signals pass through block unmodified."""
+        blk = AdafruitMotorhat()
+        self.configure_block(blk, {})
+        blk.start()
+        blk.process_signals([Signal({"hello": "n.io"})])
+        blk.stop()
+        self.assert_num_signals_notified(1)
+        self.assertDictEqual(
+            self.last_notified[DEFAULT_TERMINAL][0].to_dict(),
+            {"hello": "n.io"})
