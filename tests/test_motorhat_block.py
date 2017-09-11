abc81df3bc60bb2c6c5a0333c7d79a50bf0622f6
Scott Clay <sclay@n.io>
2017-09-11 15:53:57 -0600

    Tested

--- tests/test_motorhat_block.py
+++ tests/test_motorhat_block.py
@@ -11,7 +11,7 @@ class TestMotorhat(NIOBlockTestCase):
 
         with patch(Motorhat.__module__ + '.Adafruit_MotorHAT') as mock_MotorHAT, \
                 patch(Motorhat.__module__ + '.Adafruit_DCMotor') as mock_DCMotor:
-            mock_MotorHAT.return_value.getMotor.side_effect = [0, 1, 2, 3]
+            mock_MotorHAT.return_value.getMotor = MagicMock()
             fwd = mock_MotorHAT.FORWARD = MagicMock()
             rev = mock_MotorHAT.BACKWARD = MagicMock()
             input_signal = {'a': 205,
@@ -25,12 +25,12 @@ class TestMotorhat(NIOBlockTestCase):
                                        'motor4_speed': '{{$d}}'})
             blk.start()
             blk.process_signals([Signal(input_signal)])
-            self.assertEqual(mock_DCMotor.return_value.run.call_count, len(input_signal))
-            self.assertEqual(mock_DCMotor.return_value.getMotor.setSpeed.call_count,len(input_signal))
+            self.assertEqual(mock_MotorHAT.return_value.getMotor.return_value.run.call_count, len(input_signal))
+            self.assertEqual(mock_MotorHAT.return_value.getMotor.return_value.setSpeed.call_count,4)
             for key in input_signal:
-                mock_DCMotor.return_value.getMotor.setSpeed.assert_any_call(abs(input_signal[key]))
-                mock_DCMotor.return_value.run.assert_any_call(fwd if input_signal[key] >= 0 else rev)
+                mock_MotorHAT.return_value.getMotor.return_value.setSpeed.assert_any_call(abs(input_signal[key]))
+                mock_MotorHAT.return_value.getMotor.return_value.run.assert_any_call(fwd if input_signal[key] >= 0 else rev)
             blk.stop()
-            mock_DCMotor.return_value.setSpeed.assert_called_with(0)
-            self.assertEqual(mock_DCMotor.return_value.run.call_count, len(input_signal) * 2)
-            self.assertEqual(mock_DCMotor.return_value.getMotor.setSpeed.call_count,len(input_signal))
+            mock_MotorHAT.return_value.getMotor().setSpeed.assert_called_with(0)
+            self.assertEqual(mock_MotorHAT.return_value.getMotor.return_value.run.call_count, len(input_signal) * 2)
+            self.assertEqual(mock_MotorHAT.return_value.getMotor.return_value.setSpeed.call_count,len(input_signal) * 2)
