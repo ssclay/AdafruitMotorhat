c30e536bd0acb6356fb85b8e0965f02c968cd8b0
Scott Clay <sclay@n.io>
2017-09-11 12:02:22 -0600

    Improve Motor Numbering, and Calls

--- motorhat_block.py
+++ motorhat_block.py
@@ -6,10 +6,10 @@ from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
 class Motorhat(Block):
 
     version = VersionProperty('0.1.0')
-    motor0_speed = FloatProperty(title='DC Motor 0 Speed', default=0)
-    motor1_speed = FloatProperty(title='DC Motor 1 Speed', default=0)
-    motor2_speed = FloatProperty(title='DC Motor 2 Speed', default=0)
-    motor3_speed = FloatProperty(title='DC Motor 3 Speed', default=0)
+    motor1_speed = FloatProperty(title='DC Motor 0 Speed', default=0)
+    motor2_speed = FloatProperty(title='DC Motor 1 Speed', default=0)
+    motor3_speed = FloatProperty(title='DC Motor 2 Speed', default=0)
+    motor4_speed = FloatProperty(title='DC Motor 3 Speed', default=0)
 
     def configure(self, context):
         super().configure(context)
@@ -17,12 +17,12 @@ class Motorhat(Block):
 
     def process_signals(self, signals):
         for signal in signals:
-            for r in range(4):
+            for r in range(1, 5):
                 speed = getattr(self, 'motor{}_speed'.format(r))(signal)
                 direction = Adafruit_MotorHAT.FORWARD if speed >= 0 \
                                     else Adafruit_MotorHAT.BACKWARD
                 Adafruit_DCMotor(0, r).run(direction)
-                Adafruit_DCMotor(0, r).setSpeed(abs(speed))
+                Adafruit_DCMotor(0, r).getMotor.setSpeed(abs(speed))
 
     def stop(self):
         for r in range(4):
--- tests/test_motorhat_block.py
+++ tests/test_motorhat_block.py
@@ -19,18 +19,18 @@ class TestMotorhat(NIOBlockTestCase):
                             'c': 0,
                             'd': 0}
             blk = Motorhat()
-            self.configure_block(blk, {'motor0_speed': '{{$a}}',
-                                       'motor1_speed': '{{$b}}',
-                                       'motor2_speed': '{{$c}}',
-                                       'motor3_speed': '{{$d}}'})
+            self.configure_block(blk, {'motor1_speed': '{{$a}}',
+                                       'motor2_speed': '{{$b}}',
+                                       'motor3_speed': '{{$c}}',
+                                       'motor4_speed': '{{$d}}'})
             blk.start()
             blk.process_signals([Signal(input_signal)])
             self.assertEqual(mock_DCMotor.return_value.run.call_count, len(input_signal))
-            self.assertEqual(mock_DCMotor.return_value.setSpeed.call_count,len(input_signal))
+            self.assertEqual(mock_DCMotor.return_value.getMotor.setSpeed.call_count,len(input_signal))
             for key in input_signal:
-                mock_DCMotor.return_value.setSpeed.assert_any_call(abs(input_signal[key]))
+                mock_DCMotor.return_value.getMotor.setSpeed.assert_any_call(abs(input_signal[key]))
                 mock_DCMotor.return_value.run.assert_any_call(fwd if input_signal[key] >= 0 else rev)
             blk.stop()
             mock_DCMotor.return_value.setSpeed.assert_called_with(0)
             self.assertEqual(mock_DCMotor.return_value.run.call_count, len(input_signal) * 2)
-            self.assertEqual(mock_DCMotor.return_value.setSpeed.call_count,len(input_signal) * 2)
+            self.assertEqual(mock_DCMotor.return_value.getMotor.setSpeed.call_count,len(input_signal))
